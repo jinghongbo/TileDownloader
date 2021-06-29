d680a83eae95a51e8255bb5eb4dbc9852a5291c9
敬洪波 <jinghongbo@live.com>
2021-06-29 15:20:54 +0800

    update

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Prism.Ioc;
 using System.Windows;
 using MapDownloader.Views;
+using System.Threading.Tasks;
+using System;
+using System.Windows.Threading;
 
 namespace MapDownloader
 {
@@ -11,12 +14,44 @@ namespace MapDownloader
     {
         protected override Window CreateShell()
         {
+            //UI线程未捕获异常处理事件（UI主线程）
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+            //非UI线程未捕获异常处理事件(例如自己创建的一个子线程)
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            //Task线程内未捕获异常处理事件
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
             return Container.Resolve<MainWindow>();
+        } 
+
+        //UI线程未捕获异常处理事件（UI主线程）
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.Exception;
+            string msg = string.Format("{0}\n\n{1}", ex.Message, ex.StackTrace);//异常信息 和 调用堆栈信息
+            MessageBox.Show(msg, "UI线程异常");
+
+            e.Handled = true;//表示异常已处理，可以继续运行
         }
 
-        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        //非UI线程未捕获异常处理事件(例如自己创建的一个子线程)
+        //如果UI线程异常DispatcherUnhandledException未注册，则如果发生了UI线程未处理异常也会触发此异常事件
+        //此机制的异常捕获后应用程序会直接终止。没有像DispatcherUnhandledException事件中的Handler=true的处理方式，可以通过比如Dispatcher.Invoke将子线程异常丢在UI主线程异常处理机制中处理
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
+            Exception ex = e.ExceptionObject as Exception;
+            if (ex != null)
+            {
+                string msg = string.Format("{0}\n\n{1}", ex.Message, ex.StackTrace);//异常信息 和 调用堆栈信息
+                MessageBox.Show(msg, "非UI线程异常");
+            }
+        }
 
+        //Task线程内未捕获异常处理事件
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Exception ex = e.Exception;
+            string msg = string.Format("{0}\n\n{1}", ex.Message, ex.StackTrace);
+            MessageBox.Show(msg, "Task异常");
         }
     }
 }
